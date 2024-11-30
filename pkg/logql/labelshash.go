840f29e1b176c93a00c4ca85c46aa814147d9689
Ronan Harmegnies <ronan.harmegnies@gmail.com>
2024-11-30 17:55:21 +0100

    Minor labels hash improvement

--- pkg/logql/labelshash.go
+++ pkg/logql/labelshash.go
@@ -7,18 +7,17 @@ import (
 
 const (
 	labelSep   = '\xfe'
+	sep        = '\xff'
 	MetricName = "__name__"
 )
-
-var seps = []byte{'\xff'}
 
 func HashLabels(b []byte, ls labels.Labels) (uint64, []byte) {
 	b = b[:0]
 	for _, v := range ls {
 		b = append(b, v.Name...)
-		b = append(b, seps[0])
+		b = append(b, sep)
 		b = append(b, v.Value...)
-		b = append(b, seps[0])
+		b = append(b, sep)
 	}
 	return xxhash.Sum64(b), b
 }
@@ -34,9 +33,9 @@ func HashWithoutLabels(b []byte, ls labels.Labels, names ...string) (uint64, []b
 			continue
 		}
 		b = append(b, ls[i].Name...)
-		b = append(b, seps[0])
+		b = append(b, sep)
 		b = append(b, ls[i].Value...)
-		b = append(b, seps[0])
+		b = append(b, sep)
 	}
 	return xxhash.Sum64(b), b
 }
@@ -47,9 +46,9 @@ func HashForLabels(b []byte, ls labels.Labels, names ...string) (uint64, []byte)
 	for i < len(ls) && j < len(names) {
 		if names[j] == ls[i].Name {
 			b = append(b, ls[i].Name...)
-			b = append(b, seps[0])
+			b = append(b, sep)
 			b = append(b, ls[i].Value...)
-			b = append(b, seps[0])
+			b = append(b, sep)
 			i++
 			j++
 		} else if ls[i].Name < names[j] {
