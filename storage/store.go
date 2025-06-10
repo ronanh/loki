b5401603c5fec1b30a34da6dabedb8bbc74da75e
midnightexigent <36641328+midnightexigent@users.noreply.github.com>
2025-06-10 15:56:49 +0200

    more cleanup for storage/ moduel

--- storage/store.go
+++ storage/store.go
@@ -3,7 +3,6 @@ package storage
 import (
 	"context"
 
-	"github.com/cortexproject/cortex/pkg/chunk"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
 
@@ -12,33 +11,12 @@ import (
 	"github.com/ronanh/loki/logql"
 )
 
-type CortexStoreCommon interface {
-	Put(ctx context.Context, chunks []chunk.Chunk) error
-	PutOne(ctx context.Context, from, through model.Time, chunk chunk.Chunk) error
-	Get(ctx context.Context, userID string, from, through model.Time, matchers ...*labels.Matcher) ([]chunk.Chunk, error)
-	// GetChunkRefs returns the un-loaded chunks and the fetchers to be used to load them. You can load each slice of chunks ([]Chunk),
-	// using the corresponding Fetcher (fetchers[i].FetchChunks(ctx, chunks[i], ...)
-	GetChunkRefs(ctx context.Context, userID string, from, through model.Time, matchers ...*labels.Matcher) ([][]chunk.Chunk, []*chunk.Fetcher, error)
-	GetChunkFetcher(tm model.Time) *chunk.Fetcher
-
-	// DeleteChunk deletes a chunks index entry and then deletes the actual chunk from chunk storage.
-	// It takes care of chunks which are deleting partially by creating and inserting a new chunk first and then deleting the original chunk
-	DeleteChunk(ctx context.Context, from, through model.Time, userID, chunkID string, metric labels.Labels, partiallyDeletedInterval *model.Interval) error
-	// DeleteSeriesIDs is only relevant for SeriesStore.
-	DeleteSeriesIDs(ctx context.Context, from, through model.Time, userID string, metric labels.Labels) error
-	Stop()
-}
-
 // Store is the Loki chunk store to retrieve and save chunks.
 type Store interface {
-	// chunk.Store // remove dependency on chunk.Store (cortex)
-	CortexStoreCommon
-
 	LabelValuesForMetricName(ctx context.Context, userID string, from, through model.Time, metricName string, labelName string, matchers ...*labels.Matcher) ([]string, error)
 	LabelNamesForMetricName(ctx context.Context, userID string, from, through model.Time, metricName string, matchers ...*labels.Matcher) ([]string, error)
 
 	SelectSamples(ctx context.Context, req logql.SelectSampleParams) (iter.SampleIterator, error)
 	SelectLogs(ctx context.Context, req logql.SelectLogParams) (iter.EntryIterator, error)
 	GetSeries(ctx context.Context, req logql.SelectLogParams) ([]logproto.SeriesIdentifier, error)
-	GetSchemaConfigs() []chunk.PeriodConfig
 }
