bd966714d1703fa58e0954a588d957197d49ca58
midnightexigent <36641328+midnightexigent@users.noreply.github.com>
2025-06-19 15:55:10 +0200

    remove useless fields in config

--- querier/querier.go
+++ querier/querier.go
@@ -10,36 +10,17 @@ import (
 	"github.com/ronanh/loki/logql"
 )
 
-const (
-	// How long the Tailer should wait - once there are no entries to read from ingesters -
-	// before checking if a new entry is available (to avoid spinning the CPU in a continuous
-	// check loop)
-	tailerWaitEntryThrottle = time.Second / 2
-)
-
-type interval struct {
-	start, end time.Time
-}
-
 // Config for a querier.
 type Config struct {
-	QueryTimeout                  time.Duration    `yaml:"query_timeout"`
-	TailMaxDuration               time.Duration    `yaml:"tail_max_duration"`
-	ExtraQueryDelay               time.Duration    `yaml:"extra_query_delay,omitempty"`
-	QueryIngestersWithin          time.Duration    `yaml:"query_ingesters_within,omitempty"`
-	IngesterQueryStoreMaxLookback time.Duration    `yaml:"-"`
-	Engine                        logql.EngineOpts `yaml:"engine,omitempty"`
-	MaxConcurrent                 int              `yaml:"max_concurrent"`
+	QueryTimeout         time.Duration    `yaml:"query_timeout"`
+	QueryIngestersWithin time.Duration    `yaml:"query_ingesters_within,omitempty"`
+	Engine               logql.EngineOpts `yaml:"engine,omitempty"`
 }
 
 // RegisterFlags register flags.
 func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	cfg.Engine.RegisterFlagsWithPrefix("querier", f)
-	f.DurationVar(&cfg.TailMaxDuration, "querier.tail-max-duration", 1*time.Hour, "Limit the duration for which live tailing request would be served")
 	f.DurationVar(&cfg.QueryTimeout, "querier.query-timeout", 1*time.Minute, "Timeout when querying backends (ingesters or storage) during the execution of a query request")
-	f.DurationVar(&cfg.ExtraQueryDelay, "querier.extra-query-delay", 0, "Time to wait before sending more than the minimum successful query requests.")
-	f.DurationVar(&cfg.QueryIngestersWithin, "querier.query-ingesters-within", 0, "Maximum lookback beyond which queries are not sent to ingester. 0 means all queries are sent to ingester.")
-	f.IntVar(&cfg.MaxConcurrent, "querier.max-concurrent", 20, "The maximum number of concurrent queries.")
 }
 
 // Querier handlers queries.
