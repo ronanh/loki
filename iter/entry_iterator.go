91d2ec7e428f05154b3fd29faf10661eb1c9b6f5
JABARI Walid <wji2@3ds.com>
2025-07-24 11:52:53 +0200

    remove more dead code

--- iter/entry_iterator.go
+++ iter/entry_iterator.go
@@ -1,18 +1,15 @@
 package iter
 
 import (
-	"container/heap"
 	"context"
 	"errors"
 	"io"
-	"log/slog"
 	"sort"
 	"sync"
 	"time"
 
 	"github.com/ronanh/loki/logproto"
 	"github.com/ronanh/loki/logql/stats"
-	"github.com/ronanh/loki/util"
 )
 
 // EntryIterator iterates over entries in time-order.
@@ -72,63 +69,6 @@ func (i *streamIterator) Close() error {
 	return nil
 }
 
-type iteratorHeap []EntryIterator
-
-func (h iteratorHeap) Len() int            { return len(h) }
-func (h iteratorHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h iteratorHeap) Peek() EntryIterator { return h[0] }
-func (h *iteratorHeap) Push(x interface{}) {
-	*h = append(*h, x.(EntryIterator))
-}
-
-func (h *iteratorHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-type iteratorMinHeap struct {
-	iteratorHeap
-}
-
-func (h iteratorMinHeap) Less(i, j int) bool {
-	t1, t2 := h.iteratorHeap[i].Entry().Timestamp, h.iteratorHeap[j].Entry().Timestamp
-
-	un1 := t1.UnixNano()
-	un2 := t2.UnixNano()
-
-	switch {
-	case un1 < un2:
-		return true
-	case un1 > un2:
-		return false
-	default: // un1 == un2:
-		return h.iteratorHeap[i].Labels() < h.iteratorHeap[j].Labels()
-	}
-}
-
-type iteratorMaxHeap struct {
-	iteratorHeap
-}
-
-func (h iteratorMaxHeap) Less(i, j int) bool {
-	t1, t2 := h.iteratorHeap[i].Entry().Timestamp, h.iteratorHeap[j].Entry().Timestamp
-
-	un1 := t1.UnixNano()
-	un2 := t2.UnixNano()
-
-	switch {
-	case un1 < un2:
-		return false
-	case un1 > un2:
-		return true
-	default: // un1 == un2
-		return h.iteratorHeap[i].Labels() < h.iteratorHeap[j].Labels()
-	}
-}
-
 // HeapIterator iterates over a heap of iterators with ability to push new iterators and get some
 // properties like time of entry at peek and len
 //
@@ -141,59 +81,10 @@ type HeapIterator interface {
 }
 
 // heapIterator iterates over a heap of iterators.
-type heapIterator struct {
-	heap interface {
-		heap.Interface
-		Peek() EntryIterator
-	}
-	is         []EntryIterator
-	prefetched bool
-	stats      *stats.ChunkData
-
-	tuples     []tuple
-	currEntry  logproto.Entry
-	currLabels string
-	errs       []error
-}
-
-// NewHeapIterator returns a new iterator which uses a heap to merge together
-// entries for multiple interators.
-func NewHeapIteratorLoki(
-	ctx context.Context,
-	is []EntryIterator,
-	direction logproto.Direction,
-) HeapIterator {
-	result := &heapIterator{is: is, stats: stats.GetChunkData(ctx)}
-	switch direction {
-	case logproto.BACKWARD:
-		result.heap = &iteratorMaxHeap{}
-	case logproto.FORWARD:
-		result.heap = &iteratorMinHeap{}
-	default:
-		panic("bad direction")
-	}
-
-	result.tuples = make([]tuple, 0, len(is))
-	return result
-}
 
 // prefetch iterates over all inner iterators to merge together, calls Next() on
 // each of them to prefetch the first entry and pushes of them - who are not
 // empty - to the heap.
-func (i *heapIterator) prefetch() {
-	if i.prefetched {
-		return
-	}
-
-	i.prefetched = true
-	for _, it := range i.is {
-		i.requeue(it, false)
-	}
-
-	// We can now clear the list of input iterators to merge, given they have all
-	// been processed and the non empty ones have been pushed to the heap
-	i.is = nil
-}
 
 // requeue pushes the input ei EntryIterator to the heap, advancing it via an ei.Next()
 // call unless the advanced input parameter is true. In this latter case it expects that
@@ -201,127 +92,10 @@ func (i *heapIterator) prefetch() {
 //
 // If the iterator has no more entries or an error occur while advancing it, the iterator
 // is not pushed to the heap and any possible error captured, so that can be get via Error().
-func (i *heapIterator) requeue(ei EntryIterator, advanced bool) {
-	if advanced || ei.Next() {
-		heap.Push(i.heap, ei)
-		return
-	}
-
-	if err := ei.Error(); err != nil {
-		i.errs = append(i.errs, err)
-	}
-	if err := ei.Close(); err != nil {
-		slog.Error("closing iterator", "err", err)
-	}
-}
-
-func (i *heapIterator) Push(ei EntryIterator) {
-	i.requeue(ei, false)
-}
 
 type tuple struct {
 	logproto.Entry
 	EntryIterator
-}
-
-func (i *heapIterator) Next() bool {
-	i.prefetch()
-
-	if i.heap.Len() == 0 {
-		return false
-	}
-
-	// We support multiple entries with the same timestamp, and we want to
-	// preserve their original order. We look at all the top entries in the
-	// heap with the same timestamp, and pop the ones whose common value
-	// occurs most often.
-	for i.heap.Len() > 0 {
-		next := i.heap.Peek()
-		entry := next.Entry()
-		if len(i.tuples) > 0 &&
-			(i.tuples[0].Labels() != next.Labels() || !i.tuples[0].Timestamp.Equal(entry.Timestamp)) {
-			break
-		}
-
-		heap.Pop(i.heap)
-		i.tuples = append(i.tuples, tuple{
-			Entry:         entry,
-			EntryIterator: next,
-		})
-	}
-
-	// shortcut if we have a single tuple.
-	if len(i.tuples) == 1 {
-		i.currEntry = i.tuples[0].Entry
-		i.currLabels = i.tuples[0].Labels()
-		i.requeue(i.tuples[0].EntryIterator, false)
-		i.tuples = i.tuples[:0]
-		return true
-	}
-
-	// Find in tuples which entry occurs most often which, due to quorum based
-	// replication, is guaranteed to be the correct next entry.
-	t := i.tuples[0]
-	i.currEntry = t.Entry
-	i.currLabels = t.Labels()
-
-	// Requeue the iterators, advancing them if they were consumed.
-	for j := range i.tuples {
-		if i.tuples[j].Line != i.currEntry.Line {
-			i.requeue(i.tuples[j].EntryIterator, true)
-			continue
-		}
-		// we count as duplicates only if the tuple is not the one (t) used to fill the current
-		// entry
-		if i.tuples[j] != t {
-			i.stats.TotalDuplicates++
-		}
-		i.requeue(i.tuples[j].EntryIterator, false)
-	}
-	i.tuples = i.tuples[:0]
-	return true
-}
-
-func (i *heapIterator) Entry() logproto.Entry {
-	return i.currEntry
-}
-
-func (i *heapIterator) Labels() string {
-	return i.currLabels
-}
-
-func (i *heapIterator) Error() error {
-	switch len(i.errs) {
-	case 0:
-		return nil
-	case 1:
-		return i.errs[0]
-	default:
-		return util.MultiError(i.errs)
-	}
-}
-
-func (i *heapIterator) Close() error {
-	for i.heap.Len() > 0 {
-		if err := i.heap.Pop().(EntryIterator).Close(); err != nil {
-			return err
-		}
-	}
-	i.tuples = nil
-	return nil
-}
-
-func (i *heapIterator) Peek() time.Time {
-	i.prefetch()
-
-	return i.heap.Peek().Entry().Timestamp
-}
-
-// Len returns the number of inner iterators on the heap, still having entries.
-func (i *heapIterator) Len() int {
-	i.prefetch()
-
-	return i.heap.Len()
 }
 
 type mergingIterator struct {
@@ -593,78 +367,8 @@ type entryWithLabels struct {
 	labels string
 }
 
-type reverseIterator struct {
-	iter              EntryIterator
-	cur               entryWithLabels
-	entriesWithLabels []entryWithLabels
-
-	loaded bool
-	limit  uint32
-}
-
-// NewReversedIter returns an iterator which loads all or up to N entries
-// of an existing iterator, and then iterates over them backward.
-// Preload entries when they are being queried with a timeout.
-func NewReversedIter(it EntryIterator, limit uint32, preload bool) (EntryIterator, error) {
-	iter, err := &reverseIterator{
-		iter:              it,
-		entriesWithLabels: make([]entryWithLabels, 0, 1024),
-		limit:             limit,
-	}, it.Error()
-	if err != nil {
-		return nil, err
-	}
-
-	if preload {
-		iter.load()
-	}
-
-	return iter, nil
-}
-
-func (i *reverseIterator) load() {
-	if !i.loaded {
-		i.loaded = true
-		for count := uint32(0); (i.limit == 0 || count < i.limit) && i.iter.Next(); count++ {
-			i.entriesWithLabels = append(
-				i.entriesWithLabels,
-				entryWithLabels{i.iter.Entry(), i.iter.Labels()},
-			)
-		}
-		i.iter.Close()
-	}
-}
-
-func (i *reverseIterator) Next() bool {
-	i.load()
-	if len(i.entriesWithLabels) == 0 {
-		i.entriesWithLabels = nil
-		return false
-	}
-	i.cur = i.entriesWithLabels[len(i.entriesWithLabels)-1]
-	i.entriesWithLabels = i.entriesWithLabels[:len(i.entriesWithLabels)-1]
-	return true
-}
-
-func (i *reverseIterator) Entry() logproto.Entry {
-	return i.cur.entry
-}
-
-func (i *reverseIterator) Labels() string {
-	return i.cur.labels
-}
-
-func (i *reverseIterator) Error() error { return nil }
-
-func (i *reverseIterator) Close() error {
-	if !i.loaded {
-		return i.iter.Close()
-	}
-	return nil
-}
-
 var entryBufferPool = sync.Pool{
-	New: func() interface{} {
+	New: func() any {
 		return &entryBuffer{
 			entries: make([]entryWithLabels, 0, 1024),
 		}
@@ -673,71 +377,6 @@ var entryBufferPool = sync.Pool{
 
 type entryBuffer struct {
 	entries []entryWithLabels
-}
-
-type reverseEntryIterator struct {
-	iter EntryIterator
-	cur  entryWithLabels
-	buf  *entryBuffer
-
-	loaded bool
-}
-
-// NewEntryReversedIter returns an iterator which loads all entries and iterates backward.
-// The labels of entries is always empty.
-func NewEntryReversedIter(it EntryIterator) (EntryIterator, error) {
-	iter, err := &reverseEntryIterator{
-		iter: it,
-		buf:  entryBufferPool.Get().(*entryBuffer),
-	}, it.Error()
-	if err != nil {
-		return nil, err
-	}
-
-	return iter, nil
-}
-
-func (i *reverseEntryIterator) load() {
-	if !i.loaded {
-		i.loaded = true
-		for i.iter.Next() {
-			i.buf.entries = append(i.buf.entries, entryWithLabels{i.iter.Entry(), i.iter.Labels()})
-		}
-		i.iter.Close()
-	}
-}
-
-func (i *reverseEntryIterator) Next() bool {
-	i.load()
-	if len(i.buf.entries) == 0 {
-		entryBufferPool.Put(i.buf)
-		i.buf.entries = nil
-		return false
-	}
-	i.cur, i.buf.entries = i.buf.entries[len(i.buf.entries)-1], i.buf.entries[:len(i.buf.entries)-1]
-	return true
-}
-
-func (i *reverseEntryIterator) Entry() logproto.Entry {
-	return i.cur.entry
-}
-
-func (i *reverseEntryIterator) Labels() string {
-	return i.cur.labels
-}
-
-func (i *reverseEntryIterator) Error() error { return nil }
-
-func (i *reverseEntryIterator) Close() error {
-	if i.buf.entries != nil {
-		i.buf.entries = i.buf.entries[:0]
-		entryBufferPool.Put(i.buf)
-		i.buf.entries = nil
-	}
-	if !i.loaded {
-		return i.iter.Close()
-	}
-	return nil
 }
 
 // ReadBatch reads a set of entries off an iterator.
